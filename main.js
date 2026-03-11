42172f223ccd0876cbd913e6fac7ca16102fd9df
zyrridian <rezkyaf246@gmail.com>
2026-03-11 09:40:53 +0900

    add initial HTML and JavaScript files for graphic programming project

---
+++ main.js
@@ -0,0 +1,29 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 5);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// cube.position.set(-2, -2, 0);
+camera.position.z = 5;
+
+function animate(time) {
+  cube.rotation.x = time / 2000;
+  cube.rotation.y = time / 1000;
+  renderer.render(scene, camera);
+}
